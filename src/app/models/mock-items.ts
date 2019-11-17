b36904caefea66148e3499a47e9ef39211f9e7f9
Iris Fang <iris@dhcp-100-67.eduroam.aalto.fi>
2019-11-17 09:57:36 +0200

    Fix ids and add more products

--- src/app/models/mock-items.ts
+++ src/app/models/mock-items.ts
@@ -27,5 +27,32 @@ export const ITEMS: Item[] = [
     imgs: ['assets/imgs/cardigan.png'],
     sizes: [34, 35, 36, 37],
     colors: ['Navy', 'Burgendy'],
+  },
+  {
+    id: 'test_merchant_id_4',
+    name: 'LISI - Classic coat',
+    price: 101.99,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    imgs: ['assets/imgs/2Brown_coat.png'],
+    sizes: [34, 35, 36, 37],
+    colors: ['Caramel'],
+  },
+  {
+    id: 'test_merchant_id_5',
+    name: 'Classic coat',
+    price: 151.99,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    imgs: ['assets/imgs/3khaki_coat.png'],
+    sizes: [34, 35, 36, 37],
+    colors: ['Khaki'],
+  },
+  {
+    id: 'test_merchant_id_6',
+    name: '4black_coat.png',
+    price: 69.99,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    imgs: ['assets/imgs/4black_coat.png'],
+    sizes: [34, 35, 36, 37],
+    colors: ['Black'],
   }
 ];
--- src/app/models/mock-orders.ts
+++ src/app/models/mock-orders.ts
@@ -2,24 +2,24 @@ import { Order } from './order';
 
 export const ORDERS: Order[] = [
   {
-    id: 'test_merchant_id_1',
-    products: ['test_product_id_1'],
+    id: 'test_order_id_1',
+    products: ['test_merchant_id_1'],
     image: 'assets/imgs/cloth.png',
     date: '31-10-19',
     status: 'shipped',
     sizes: [34]
   },
   {
-    id: 'test_merchant_id_2',
-    products: ['test_product_id_2', 'test_product_id_3'],
+    id: 'test_order_id_2',
+    products: ['test_merchant_id_2', 'test_merchant_id_3'],
     image: 'assets/imgs/jacket.png',
     date: '27-09-19',
     status: 'received',
-    sizes: [32, 34]
+    sizes: [34, 35]
   },
   {
-    id: 'test_merchant_id_3',
-    products: ['test_product_id_2', 'test_product_id_3'],
+    id: 'test_order_id_3',
+    products: ['test_merchant_id_2', 'test_merchant_id_3'],
     image: 'assets/imgs/cardigan.png',
     date: '12-08-19',
     status: 'received',
